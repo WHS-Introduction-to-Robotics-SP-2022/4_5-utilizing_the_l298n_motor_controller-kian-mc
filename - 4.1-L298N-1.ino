91e426099cb563a8ec78a7384cbb997a3bba53a8
kian-mc <71298052+kian-mc@users.noreply.github.com>
2022-04-25 13:17:01 -0600

    Create - 4.1-L298N-1.ino

---
+++ - 4.1-L298N-1.ino
@@ -0,0 +1,20 @@
+
+/*
+
+*/
+
+
+void setup() {
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(11,OUTPUT);
+  digitalWrite(9,HIGH);
+  analogWrite(11, 127);
+  delay(5000);
+  analogWrite(11, 0);
+}
+
+void loop() {
+  
+  
+}
